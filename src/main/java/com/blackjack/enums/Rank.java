0c6198bbe2dcfab32a3f8c5c5153084b070e5bcb
p0pkern <petermanchristoph@gmail.com>
2023-05-24 12:38:33 -0400

    Added enums for card suit and rank

---
+++ src/main/java/com/blackjack/enums/Rank.java
@@ -0,0 +1,18 @@
+package com.blackjack.enums;
+
+public enum Rank {
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING,
+	ACE,
+	RANK_SIZE
+}
---
+++ src/main/java/com/blackjack/enums/Suit.java
@@ -0,0 +1,9 @@
+package com.blackjack.enums;
+
+public enum Suit {
+	SPADE,
+	HEART,
+	CLUB,
+	DIAMOND,
+	SUIT_SIZE
+}
